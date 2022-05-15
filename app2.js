bef0a94b264b425337e2406efc52379472b0d2e2
liyic25 <liyic25@gmail.com>
2022-05-15 13:39:41 +0800

    bug fixed

--- app2.js
+++ app2.js
@@ -211,164 +211,209 @@ const wordInPlay = words[wordIndex].toUpperCase();
 console.log("InPlay:", wordInPlay);
 
 const keys = [
-    'Q','W','E','R','T','Y','U','I','O','P',
-    'A','S','D','F','G','H','J','K','L',
-    'DEL','Z','X','C','V','B','N','M','ENTER'
-]
+  "Q",
+  "W",
+  "E",
+  "R",
+  "T",
+  "Y",
+  "U",
+  "I",
+  "O",
+  "P",
+  "A",
+  "S",
+  "D",
+  "F",
+  "G",
+  "H",
+  "J",
+  "K",
+  "L",
+  "DEL",
+  "Z",
+  "X",
+  "C",
+  "V",
+  "B",
+  "N",
+  "M",
+  "ENTER",
+];
 const rows = [
-    ['','','','',''],
-    ['','','','',''],
-    ['','','','',''],
-    ['','','','',''],
-    ['','','','',''],
-    ['','','','','']
-]
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
 
-let currentRow = 0
-let currentInputBox = 0
+let currentRow = 0;
+let currentInputBox = 0;
 
 $(() => {
-    // Extra for textbook learning:
-    // const makeKeymap = () => {
-        //     keys.forEach((alphabet => {
-            //         $key= $("<button>").text(alphabet)
-            //         $(".key-container").append($key)
-            //     }))
-// }
-// makeKeymap();
+  // Extra for textbook learning:
+  // const makeKeymap = () => {
+  //     keys.forEach((alphabet => {
+  //         $key= $("<button>").text(alphabet)
+  //         $(".key-container").append($key)
+  //     }))
+  // }
+  // makeKeymap();
 
-// Make Keyboard
-const makeKey = () => {
+  // Make Keyboard
+  const makeKey = () => {
     for (const key of keys) {
-        $key= $("<button>").text(key).attr("id", key).addClass("button")
-        $(".key-container").append($key)
+      $key = $("<button>").text(key).attr("id", key).addClass("button");
+      $(".key-container").append($key);
     }
-}
-makeKey();
+  };
+  makeKey();
 
-// $(".button").on("click", (event) => {
-    // will cause all to do print// for (key of keys)
-    // unnecessary. just nest // handleKeyClick(event.target.id);
-    //     console.log(event.target.id)
-    // })
+  // $(".button").on("click", (event) => {
+  // will cause all to do print// for (key of keys)
+  // unnecessary. just nest // handleKeyClick(event.target.id);
+  //     console.log(event.target.id)
+  // })
 
-    // Unnessary 
-    // const handleKeyClick = (event) => {
-        //     console.log("clicked!" + event.target)
-// }
+  // Unnessary
+  // const handleKeyClick = (event) => {
+  //     console.log("clicked!" + event.target)
+  // }
 
-// Create Board, Row, Element, Button
-const makeRows = () => {
-    for (let i=0; i< rows.length; i++) {
-        $row= $("<div>").addClass("row").attr("id", `row${i}`)
-                $(".input-container").append($row)
-                // Make Input Box
-                for (let j=0; j<5; j++) {
-                    $input= $("<div>").addClass("input").attr("id", `row${i}box${j}`)
-                    $row.append($input)
-                }}}
-makeRows()
-
-// Extra for textbook learning:
-// const makeRowsmap = (row, index) => {
-//     rows.forEach((row => {
-    //         $row= $("<div>").attr("id", row.id) // what is the index?
-    //         $(".input-container").append($row)
-    //     }))
-    // }
-    
-    // makeRowsmap()
-    
-    $(".button").on("click", (event) => {
-        if (event.target.id === "DEL") {
-            deleteLetter()
-        } else
-        if (event.target.id === "ENTER") {
-            checkRow()
-        } else {
-        AddLetter(event.target.id)}
-    })
-
-    const AddLetter = (key) => {
-        if (currentRow < 6 && currentInputBox < 5 ) {
-        let x = `#row${currentRow}box${currentInputBox}`  
-        console.log("x:", x)
-        $(x).text(key)
-        //$(x).attr("data", key)
-        rows[currentRow][currentInputBox] = key
-        console.log(rows)
-        currentInputBox++
-        }
+  // Create Board, Row, Element, Button
+  const makeRows = () => {
+    for (let i = 0; i < rows.length; i++) {
+      $row = $("<div>").addClass("row").attr("id", `row${i}`);
+      $(".input-container").append($row);
+      // Make Input Box
+      for (let j = 0; j < 5; j++) {
+        $input = $("<div>").addClass("input").attr("id", `row${i}box${j}`);
+        $row.append($input);
+      }
     }
-    const deleteLetter = () => {
-        if(currentInputBox > 0){
-        currentInputBox--
-        let y = `#row${currentRow}box${currentInputBox}`  
-        $(y).text("")
-       // $(y).data("")
-        rows[currentRow][currentInputBox] = ""
-        }
-        console.log(rows)
-    }  
-    const checkRow = () => {
-        if (currentInputBox > 4) { // it never exceeds 5
-            let guessInPlay = rows[currentRow].join('')
-            console.log("p: ",wordInPlay, guessInPlay)
-            flipInputBox(currentRow)
+  };
+  makeRows();
 
-            if (wordInPlay === guessInPlay) {
-                showMessage("Correct!")
-                refreshPageOption()
+  // Extra for textbook learning:
+  // const makeRowsmap = (row, index) => {
+  //     rows.forEach((row => {
+  //         $row= $("<div>").attr("id", row.id) // what is the index?
+  //         $(".input-container").append($row)
+  //     }))
+  // }
 
-            } else 
-                if (currentRow < 5 ) {
-                currentRow ++
-                currentInputBox = 0
-                } else {
-                showMessage("Try again!")
-                refreshPageOption()
-                }
-        }
-    }
-    const showMessage = (msg) => {
-        // $message = $("<p>").text(msg)
-        // $(".message-container").append($message) 
-        $("h1").text(msg)
-    }
-    const refreshPageOption =() => {
-        $refresh = $("<button>").addClass("refresh").text("Click here to play again")
-        $(".message-container").append($refresh) 
+  // makeRowsmap()
 
-        $refresh.on("click", ()=> {
-            location.reload()
-        })
+  $(".button").on("click", (event) => {
+    if (event.target.id === "DEL") {
+      deleteLetter();
+    } else if (event.target.id === "ENTER") {
+      checkRow();
+    } else {
+      AddLetter(event.target.id);
     }
-    const flipInputBox = (currentRow) => {
-        const M = `#row${currentRow}`
-        let rowOfInputs = $(M).children().text() // this is a string
-        let guess = [] // this is an array
-        
-        for (let i=0; i<5; i++) {
-            const N = `#row${currentRow}box${i}`
-            const Z = rowOfInputs[i]
-            const K = `#${Z}`
-            guess.push(Z)
-            
-            if (wordInPlay[i]===rowOfInputs[i]) {
-                $(N).addClass("flipGreen")
-                $(K).addClass("flipGrey")
-                guess[i] = ''
-            } else 
-            if (wordInPlay.includes(rowOfInputs[i]) && guess.includes(rowOfInputs[i])) {
-                console.log(`turn ${i} `, guess)
-                $(N).addClass("flipYellow")
-                $(K).addClass("flipGrey")
-                guess[i] = ''
-            } else {
-                $(N).addClass("flipGrey")
-                $(K).addClass("flipGrey")
-                guess[i] = ''
-        }
-    }}
+  });
+
+  const AddLetter = (key) => {
+    if (currentRow < 6 && currentInputBox < 5) {
+      let x = `#row${currentRow}box${currentInputBox}`;
+      console.log("x:", x);
+      $(x).text(key);
+      //$(x).attr("data", key)
+      rows[currentRow][currentInputBox] = key;
+      console.log(rows);
+      currentInputBox++;
+    }
+  };
+  const deleteLetter = () => {
+    if (currentInputBox > 0) {
+      currentInputBox--;
+      let y = `#row${currentRow}box${currentInputBox}`;
+      $(y).text("");
+      // $(y).data("")
+      rows[currentRow][currentInputBox] = "";
+    }
+    console.log(rows);
+  };
+  const checkRow = () => {
+    if (currentInputBox > 4) {
+      // it never exceeds 5
+      let guessInPlay = rows[currentRow].join("");
+      console.log("p: ", wordInPlay, guessInPlay);
+      flipInputBox(currentRow);
+
+      if (wordInPlay === guessInPlay) {
+        showMessage("Correct!");
+        refreshPageOption();
+      } else if (currentRow < 5) {
+        currentRow++;
+        currentInputBox = 0;
+      } else {
+        showMessage("Try again!");
+        refreshPageOption();
+      }
+    }
+  };
+  const showMessage = (msg) => {
+    // $message = $("<p>").text(msg)
+    // $(".message-container").append($message)
+    $("h1").text(msg);
+  };
+  const refreshPageOption = () => {
+    $refresh = $("<button>")
+      .addClass("refresh")
+      .text("Click here to play again");
+    $(".message-container").append($refresh);
+
+    $refresh.on("click", () => {
+      location.reload();
+    });
+  };
+  const flipInputBox = (currentRow) => {
+    const M = `#row${currentRow}`;
+    let rowOfInputs = $(M).children().text(); // this is a string
+    let remainder_guess = wordInPlay.split(""); // this is an array
+    console.log(rowOfInputs, remainder_guess)
+
+    for (let i = 0; i < 5; i++) {
+      const N = `#row${currentRow}box${i}`;
+      const Z = rowOfInputs[i];
+      const K = `#${Z}`;
+
+      if (wordInPlay[i] === rowOfInputs[i]) {
+        $(N).addClass("flipGreen");
+        $(K).addClass("flipGrey");
+        remainder_guess[i] = "";
+      }
+      console.log("Step1_remainderGuess", remainder_guess);
+    }
+    // return remainder_guess
+    flipInputBoxStep2(remainder_guess)
+};
+
+const flipInputBoxStep2 = (remainder_guess) => {
+  // if (guess.includes(rowOfInputs[i]) && ($(N).attr("class") != "flipGreen")) {
+
+  console.log("Step2_Guess", remainder_guess);
+  const M = `#row${currentRow}`;
+  let rowOfInputs = $(M).children().text(); // this is a string
+
+  for (let i = 0; i < 6; i++) {
+    const N = `#row${currentRow}box${i}`;
+    const Z = rowOfInputs[i];
+    const K = `#${Z}`;
+
+    if (remainder_guess.includes(rowOfInputs[i])) {
+      // console.log(i, $(N).attr("class"))
+      $(N).addClass("flipYellow");
+      $(K).addClass("flipGrey");
+    } else {
+      $(N).addClass("flipGrey");
+      $(K).addClass("flipGrey");
+    }
+  }
+};
 
 });
